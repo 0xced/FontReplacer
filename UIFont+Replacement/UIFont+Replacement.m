83298bd5a3bf7803320345a2d41c8b33492f3908
Samuel Défago <defagos@gmail.com>
2012-01-13 06:50:24 +0100

    Baseline auto-alignment of replacement font and original font

--- UIFont+Replacement/UIFont+Replacement.m
+++ UIFont+Replacement/UIFont+Replacement.m
@@ -30,11 +30,16 @@ static void initializeReplacementFonts()
 	Method fontWithName_size_traits_ = class_getClassMethod([UIFont class], @selector(fontWithName:size:traits:));
 	Method replacementFontWithName_size_ = class_getClassMethod([UIFont class], @selector(replacement_fontWithName:size:));
 	Method replacementFontWithName_size_traits_ = class_getClassMethod([UIFont class], @selector(replacement_fontWithName:size:traits:));
-	
+    
 	if (fontWithName_size_ && replacementFontWithName_size_ && strcmp(method_getTypeEncoding(fontWithName_size_), method_getTypeEncoding(replacementFontWithName_size_)) == 0)
 		method_exchangeImplementations(fontWithName_size_, replacementFontWithName_size_);
 	if (fontWithName_size_traits_ && replacementFontWithName_size_traits_ && strcmp(method_getTypeEncoding(fontWithName_size_traits_), method_getTypeEncoding(replacementFontWithName_size_traits_)) == 0)
 		method_exchangeImplementations(fontWithName_size_traits_, replacementFontWithName_size_traits_);
+    
+	Method ascender_ = class_getInstanceMethod([UIFont class], @selector(ascender));
+	Method replacementAscender_ = class_getInstanceMethod([UIFont class], @selector(replacement_ascender));
+	if (ascender_ && replacementAscender_ && strcmp(method_getTypeEncoding(ascender_), method_getTypeEncoding(replacementAscender_)) == 0)
+		method_exchangeImplementations(ascender_, replacementAscender_);
 }
 
 + (UIFont *) replacement_fontWithName:(NSString *)fontName size:(CGFloat)fontSize
@@ -49,6 +54,23 @@ static void initializeReplacementFonts()
 	initializeReplacementFonts();
 	NSString *replacementFontName = [replacementDictionary objectForKey:fontName];
 	return [self replacement_fontWithName:replacementFontName ?: fontName size:fontSize traits:traits];
+}
+
+- (CGFloat)replacement_ascender
+{
+	NSString *fontName = [self fontName];
+	CGFloat ascender = [self replacement_ascender];
+    
+	NSArray *replacedFontNames = [replacementDictionary allKeysForObject:fontName];
+	if ([replacedFontNames count] == 0) 
+	{
+		return ascender;
+	}
+    
+	NSString *replacedFontName = [replacedFontNames objectAtIndex:0];
+	NSNumber *traits = [self valueForKey:@"traits"];
+	UIFont *replacedFont = [UIFont replacement_fontWithName:replacedFontName size:self.pointSize traits:[traits unsignedIntegerValue]];
+	return [replacedFont replacement_ascender];
 }
 
 + (NSDictionary *) replacementDictionary
